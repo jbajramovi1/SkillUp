2013a2929c70fef76071bbafc5fde900cb763788
Jasmina Bajramović <bajramovicjasmina2@gmail.com>
2016-11-16 02:51:20 +0100

    Implementiran dropdown meni

--- skripta.js
+++ skripta.js
@@ -1,3 +1,10 @@
 function onclick_homeBtn(){
 	window.location="index.html";
 }
+
+function onclick_meniBtn(){
+	var meni=document.getElementById("dropdownMeni");
+	if (meni.style.visibility=='visible') {meni.style.visibility='hidden';}
+	else {meni.style.visibility='visible';}
+
+}
